94746ef5f2a678ff57ed45638a385628b586b838
shikai.hung <shikai.hung@rdgroup.com.tw>
2018-10-16 15:30:26 +0800

    Add Handler & state to make order item

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,116 @@
 import React from 'react';
-import './App.css';
+import { compose, withState, withHandlers } from 'recompose';
+import styled from 'styled-components';
+import OrderDetail from './components/OrderDetail';
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <header className="App-header" />
-      </div>
-    );
-  }
-}
+const MainComponent = ({
+  setName,
+  setIce,
+  setSugar,
+  setPrice,
+  setQuantity,
+  onSubmit,
+  order,
+}) => {
+  return (
+    <Main>
+      <OrderBox>
+        <OrderItem>
+          <input type="text" onChange={e => setName(e.target.value)} />
+          <input
+            onChange={e => setPrice(e.target.value)}
+            style={{ width: 20, margin: 10 }}
+            type="text"
+          />
+          <select onChange={e => setIce(e.target.value)}>
+            <option value="regularIce">正常</option>
+            <option value="lessIce">少冰</option>
+            <option value="noIce">去冰</option>
+            <option value="warm">溫</option>
+          </select>
+          <select onChange={e => setSugar(e.target.value)}>
+            <option value="regularSugar">正常</option>
+            <option value="halfSugar">半糖</option>
+            <option value="lessSugar">微糖</option>
+            <option value="noSugar">無糖</option>
+          </select>
+          <input
+            onChange={e => setQuantity(e.target.value)}
+            style={{ width: 50, margin: 10 }}
+            type="number"
+            placeholder="數量"
+          />
+          <button onClick={onSubmit}>保存</button>
+        </OrderItem>
+        {order.length > 0 &&
+          order.map((item, index) => (
+            <OrderDetail
+              key={index}
+              name={item.name}
+              ice={item.ice}
+              price={item.price}
+              quantity={item.quantity}
+              sugar={item.sugar}
+              total={item.total}
+            />
+          ))}
+      </OrderBox>
+    </Main>
+  );
+};
+
+const App = compose(
+  withState('order', 'setOrder', []),
+  withState('name', 'setName', ''),
+  withState('ice', 'setIce', 'regularIce'),
+  withState('sugar', 'setSugar', 'regularSugar'),
+  withState('price', 'setPrice', 0),
+  withState('quantity', 'setQuantity', 0),
+  withHandlers({
+    onSubmit: ({
+      name,
+      ice,
+      sugar,
+      price,
+      quantity,
+      order,
+      setOrder,
+    }) => () => {
+      const orderList = order;
+      const addOrder = {
+        name: name,
+        ice: ice,
+        sugar: sugar,
+        price: price,
+        quantity: quantity,
+        total: price * quantity,
+      };
+      orderList.push(addOrder);
+      setOrder(orderList);
+    },
+  })
+)(MainComponent);
 
 export default App;
+
+const Main = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #f8f8f8;
+`;
+
+const OrderBox = styled.div`
+  width: 600px;
+  height: 80%;
+  background: #fff;
+  box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+`;
+
+const OrderItem = styled.div`
+  width: 80%;
+  height: 60px;
+  padding: 10px;
+`;
---
+++ src/components/OrderDetail/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const OrderDetail = ({ price, name, sugar, ice, quantity, total }) => (
+  <Main>
+    <div>
+      <Title>{name}</Title>
+      <SubTitle>{`${sugar}/${ice}/${price}`}</SubTitle>
+    </div>
+    <Container>
+      <h6>x</h6>
+      <h6>{quantity}</h6>
+      <h6>${total}</h6>
+    </Container>
+  </Main>
+);
+
+export default OrderDetail;
+
+const Main = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  padding: 15px;
+  display: flex;
+  border-bottom: 1px solid #000;
+`;
+
+const Title = styled.p`
+  font-size: 16px;
+  margin: 0;
+  color: #000;
+`;
+
+const SubTitle = styled.span`
+  font-size: 12px;
+  color: #484848;
+`;
+
+const Container = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+  h6 {
+    color: #4a4a4a;
+    line-height: 40px;
+    margin: 0 0 0 40px;
+    font-weight: 400;
+    font-size: 24px;
+  }
+`;
