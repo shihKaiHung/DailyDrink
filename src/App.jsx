a92b5250d1a1b16b574f3ddd568c6cdf2ed0ddbc
shikai.hung <shikai.hung@rdgroup.com.tw>
2018-10-17 14:41:39 +0800

    Add Select style

--- src/App.jsx
+++ src/App.jsx
@@ -19,33 +19,63 @@ const MainComponent = ({
     <Main>
       {ModalComponent}
       <OrderBox>
-        <OrderItem>
-          <input type="text" onChange={e => setName(e.target.value)} />
-          <input
-            onChange={e => setPrice(e.target.value)}
-            style={{ width: 20, margin: 10 }}
-            type="text"
-          />
-          <select onChange={e => setIce(e.target.value)}>
-            <option value="regularIce">正常</option>
-            <option value="lessIce">少冰</option>
-            <option value="noIce">去冰</option>
-            <option value="warm">溫</option>
-          </select>
-          <select onChange={e => setSugar(e.target.value)}>
-            <option value="regularSugar">正常</option>
-            <option value="halfSugar">半糖</option>
-            <option value="lessSugar">微糖</option>
-            <option value="noSugar">無糖</option>
-          </select>
-          <input
-            onChange={e => setQuantity(e.target.value)}
-            style={{ width: 50, margin: 10 }}
-            type="number"
-            placeholder="數量"
-          />
-          <button onClick={onSubmit}>保存</button>
-        </OrderItem>
+        <OrderContainer>
+          <Title>新增訂單</Title>
+          <OrderItem>
+            <OrderItemTitle>品項</OrderItemTitle>
+            <OrderItemInput
+              type="text"
+              onChange={e => setName(e.target.value)}
+            />
+          </OrderItem>
+          <OrderItem>
+            <OrderItemTitle>價錢</OrderItemTitle>
+            <OrderItemInput
+              type="number"
+              onChange={e => setPrice(e.target.value)}
+            />
+          </OrderItem>
+          <OrderItem>
+            <OrderItemTitle>數量</OrderItemTitle>
+            <OrderItemInput
+              min="0"
+              type="number"
+              onChange={e => setQuantity(e.target.value)}
+            />
+          </OrderItem>
+          <OrderItem>
+            <OrderItemTitle>冰塊</OrderItemTitle>
+            <SelectBox>
+              <Select onChange={e => setIce(e.target.value)}>
+                <option value="正常">正常</option>
+                <option value="少冰<">少冰</option>
+                <option value="去冰">去冰</option>
+                <option value="溫">溫</option>
+              </Select>
+            </SelectBox>
+          </OrderItem>
+          <OrderItem>
+            <OrderItemTitle>甜度</OrderItemTitle>
+            <SelectBox>
+              <Select onChange={e => setSugar(e.target.value)}>
+                <option value="正常">正常</option>
+                <option value="少糖">少糖</option>
+                <option value="微糖">微糖</option>
+                <option value="無糖">無糖</option>
+              </Select>
+            </SelectBox>
+          </OrderItem>
+          <OrderItem>
+            <OrderItemTitle>備註</OrderItemTitle>
+            <textarea name="" id="" cols="30" rows="2" />
+          </OrderItem>
+          <OrderItem>
+            <Button onClick={onSubmit}>新增</Button>
+          </OrderItem>
+        </OrderContainer>
+      </OrderBox>
+      <Filldiv />
+      <OrderBox>
         {order.length > 0 &&
           order.map((item, index) => (
             <OrderDetail
@@ -69,8 +99,8 @@ const App = compose(
   withModal(),
   withState('order', 'setOrder', []),
   withState('name', 'setName', ''),
-  withState('ice', 'setIce', 'regularIce'),
-  withState('sugar', 'setSugar', 'regularSugar'),
+  withState('ice', 'setIce', '正常'),
+  withState('sugar', 'setSugar', '正常'),
   withState('price', 'setPrice', 0),
   withState('quantity', 'setQuantity', 0),
   withHandlers({
@@ -85,7 +115,7 @@ const App = compose(
       setMessage,
       openModal,
     }) => () => {
-      if (price == 0 || name == '' || quantity == 0) {
+      if (price === 0 || name === '' || quantity === 0) {
         openModal();
         return;
       }
@@ -131,15 +161,112 @@ const Main = styled.div`
   background-color: #f8f8f8;
 `;
 
+const Title = styled.h3`
+  text-align: center;
+  font-size: 18px;
+  margin-top: 5px;
+`;
+
 const OrderBox = styled.div`
-  width: 600px;
+  width: 400px;
   height: 80%;
   background: #fff;
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  overflow: auto;
+`;
+
+const Filldiv = styled.div`
+  width: 50px;
+  height: 100%;
+`;
+
+const OrderContainer = styled.div`
+  width: 60%;
+  height: 60px;
+  margin: 0 auto;
 `;
 
 const OrderItem = styled.div`
-  width: 80%;
-  height: 60px;
-  padding: 10px;
+  box-sizing: border-box;
+  width: 100%;
+  height: 80%;
+  margin-top: 5px;
+  padding: 0 20px;
+`;
+
+const OrderItemTitle = styled.p`
+  font-size: 14px;
+  margin: 0;
+  color: #a37264;
+`;
+
+const OrderItemInput = styled.input`
+  width: 100%;
+  height: 15px;
+  font-size: 16px;
+  border: none;
+  transition: 0.5s;
+  border-bottom: 1px solid #d7d4d0;
+  &:focus {
+    outline: none;
+    font-size: 18px;
+    height: 25px;
+  }
+`;
+
+const Button = styled.button`
+  margin-top: 20px;
+  cursor: pointer;
+  width: 100%;
+  height: 30px;
+  background: #fff;
+  color: #a37264;
+  outline: none;
+  border: 1px solid #3b8ec2;
+  text-align: center;
+  transition: 0.3s;
+  &:hover {
+    background-color: #3b8ec2;
+    color: #fff;
+  }
+`;
+
+const SelectBox = styled.div`
+  margin-top: 5px;
+  position: relative;
+  display: block;
+  width: 100%;
+  height: 20px;
+  line-height: 0;
+  background: #3b8ec2;
+  overflow: hidden;
+  &:after {
+    content: '\\25BC';
+    color: #fff;
+    position: absolute;
+    top: 9px;
+    right: 10px;
+    bottom: 0;
+    background: #3b8ec2;
+    pointer-events: none;
+    transition: 0.25s all ease;
+  }
+`;
+
+const Select = styled.select`
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  -webkit-font-smoothing: antialiased;
+  -ms-appearance: none;
+  appearance: none;
+  outline: 0;
+  box-shadow: none;
+  border: 0 !important;
+  background: #3b8ec2 none;
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0 0 0 0.5em;
+  color: #fff;
+  cursor: pointer;
 `;
--- src/components/OrderDetail/index.jsx
+++ src/components/OrderDetail/index.jsx
@@ -12,16 +12,19 @@ const OrderDetail = ({
   id,
 }) => (
   <Main>
-    <div>
-      <Title>{name}</Title>
-      <SubTitle>{`${sugar}/${ice}/${price}`}</SubTitle>
-    </div>
-    <Container>
-      <h6>x</h6>
-      <h6>{quantity}</h6>
-      <h6>${total}</h6>
-    </Container>
-    <button onClick={() => onDelete(id)}>delete</button>
+    <Wrap>
+      <div>
+        <Title>{name}</Title>
+        <SubTitle>{`${sugar}/${ice}/$${price}`}</SubTitle>
+      </div>
+      <Container>
+        <h6>x</h6>
+        <h6>{quantity}</h6>
+        <h6>${total}</h6>
+        <Button onClick={() => onDelete(id)}>修改</Button>
+        <Button onClick={() => onDelete(id)}>刪除</Button>
+      </Container>
+    </Wrap>
   </Main>
 );
 
@@ -31,10 +34,24 @@ const Main = styled.div`
   box-sizing: border-box;
   width: 100%;
   padding: 15px;
-  display: flex;
-  border-bottom: 1px solid #000;
 `;
 
+const Wrap = styled.div`
+  width: 100%;
+  border-bottom: 1px solid #d7d4d0;
+  display: flex;
+`
+
+const Button = styled.button`
+  cursor: pointer;
+  padding: 7px 15px;
+  border-radius: 5px;
+  margin-left: 15px;
+  background: #3b8ec2;
+  border: none;
+  color: #fff;
+  text-align: center;
+`;
 const Title = styled.p`
   font-size: 16px;
   margin: 0;
@@ -50,12 +67,11 @@ const Container = styled.div`
   height: 100%;
   display: flex;
   align-items: center;
-  padding-left: 20px;
   h6 {
     color: #4a4a4a;
     line-height: 40px;
-    margin: 0 0 0 40px;
+    margin: 0 0 0 20px;
     font-weight: 400;
-    font-size: 24px;
+    font-size: 18px;
   }
 `;
